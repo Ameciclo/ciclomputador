031d5a2f23350c8ca8f508b6aa5e48c7223d6945
Italo Sergio <italo@graduate.org>
2024-05-16 23:15:05 -0300

    adicionado: arquivos gpx

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import * as fs from 'fs';
 import * as xml2js from 'xml2js';
+import iDataForms from './interfaces/iDataForms';
+import iGPXData from './interfaces/iGPXData';
+import iGPXNode from './interfaces/iGPXNode';
+import iDataFormsMetadata from './interfaces/iDataFormsMetadata';
+import { iDuo, iDuos } from './interfaces/iDuos';
 
 const gpxFolderPath = 'src/gpx-files';
 
@@ -12,39 +17,26 @@ function gpxProcess(filePath: string) {
                 console.error('Erro ao analisar o arquivo GPX:', err);
                 return;
             }
-            // console.log(result.gpx.wpt)
-            function applyParametrization(object) {
+
+            function applyParametrization(object: iDataForms) {
                 for (const propriedade in object) {
                     if (!propriedade.includes("qte")) {
                         if (typeof object[propriedade] === "object") {
                             for (const key in object[propriedade]) {
-                                object[propriedade][key] = parametrization(result, key, "qualitativo");
+                                object[propriedade][key] = parametrization(result, key, "geral");
                             }
                         }
                     }
-                    else if (propriedade.includes("sinalizacao_horizontal")) {
-                        console.log(propriedade)
-                        if (typeof object[propriedade] === "object") {
-                            for (const key in object[propriedade]) {
-                                object[propriedade][key] = parametrization(result, key, propriedade);
-                            }
-                        }
+                    if (propriedade == "metadata") {
+                        const KEY_ON_DATA = "desc";
+                        object[propriedade] = parametrization(result, KEY_ON_DATA, "metadata");
                     }
-                    else if (propriedade.includes("larguras_estrutura")) {
-                        if (typeof object[propriedade] === "object") {
-                            for (const key in object[propriedade]) {
-                                object[propriedade][key] = parametrization(result, key, propriedade);
-                            }
-                        }
-                    }
-                    else if (propriedade.includes("controle_de_velocidade")) {
-                        if (typeof object[propriedade] === "object") {
-                            for (const key in object[propriedade]) {
-                                object[propriedade][key] = parametrization(result, key, propriedade);
-                            }
-                        }
-                    }
-                    else if (propriedade.includes("iluminacao_estrutura")) {
+                    
+                    else if (propriedade.includes("sinalizacao_horizontal") ||
+                        propriedade.includes("larguras_estrutura") ||
+                        propriedade.includes("controle_de_velocidade") ||
+                        propriedade.includes("iluminacao_estrutura") ||
+                        propriedade.includes("obstaculos")) {
                         if (typeof object[propriedade] === "object") {
                             for (const key in object[propriedade]) {
                                 object[propriedade][key] = parametrization(result, key, propriedade);
@@ -52,11 +44,32 @@ function gpxProcess(filePath: string) {
                         }
                     }
                 }
-
             }
-            const assessmentParams = JSON.parse(fs.readFileSync("src/references/params.json", 'utf-8'))
-            applyParametrization(assessmentParams)
-            console.log(assessmentParams)
+            function saveJSON(data: any, filename: string) {
+                let dataArray: any[] = [];
+
+                if (fs.existsSync(filename)) {
+                    dataArray = JSON.parse(fs.readFileSync(filename, 'utf-8'));
+                }
+
+                const newId = dataArray.length + 1;
+                const newData = { id: newId, ...data };
+                dataArray.push(newData);
+
+                fs.writeFileSync(filename, JSON.stringify(dataArray, null, 2));
+            }
+
+            const resultFolderPath = './src/result';
+            const resultFilePath = `${resultFolderPath}/data.json`;
+
+            if (!fs.existsSync(resultFolderPath)) {
+                fs.mkdirSync(resultFolderPath);
+            }
+
+            const assessmentParams = JSON.parse(fs.readFileSync("src/references/params.json", 'utf-8'));
+            applyParametrization(assessmentParams);
+            saveJSON(assessmentParams, resultFilePath);
+            console.log(`GPX ${result.gpx.metadata[0].name[0]} processado...`)
         });
     } catch (error) {
         console.error('Erro ao processar o arquivo GPX:', error);
@@ -67,7 +80,8 @@ function processarTodosArquivosGpx() {
     try {
         const fileNames = fs.readdirSync(gpxFolderPath);
 
-        console.log(`Foram encontrados ${fileNames.length} arquivos`)
+        console.log(`Foram encontrados ${fileNames.length} arquivos`);
+        console.log("Processando dados GPX")
         for (const fileName of fileNames) {
             const filePath = `${gpxFolderPath}/${fileName}`;
             gpxProcess(filePath);
@@ -77,59 +91,50 @@ function processarTodosArquivosGpx() {
     }
 }
 
-function parametrization(data: object, param: string, type: string) {
+function parametrization(data: iGPXData, param: string, type: string) {
     switch (type) {
-        case "qualitativo":
-            return data.gpx.wpt.some((elem, index) => {
+        case "metadata":
+            const codigo_da_area = data.gpx.metadata[0].desc[0].toLowerCase();
+            const dadosAreaAvaliacao = JSON.parse(fs.readFileSync("./src/references/area.json", 'utf-8'));
+            const duplas: iDuos = JSON.parse(fs.readFileSync("./src/references/duos.json", 'utf-8'));
+            const result = dadosAreaAvaliacao.find((elem: iDataFormsMetadata) => {
+                const codigo = elem.cod.toLowerCase();
+                return codigo.includes(codigo_da_area);
+            });
+            result["cod"] = codigo_da_area;
+            let names;
+            const foundDuo = duplas.find((dupla: iDuo) => dupla["cod"] === result["dupla"]);
+            if (foundDuo) {
+                names = foundDuo.names.join(" e ");
+            } else {
+                names = result["dupla"];
+            }
+            result["dupla"] = names;
+
+            return result;
+        case "geral":
+            return data.gpx.wpt.some((elem: iGPXNode, index: number) => {
                 if (!!data.gpx.wpt[index + 1]) {
                     if (!data.gpx.wpt[index + 1].name.includes("Remover")) {
                         return elem.name[0] === param
                     }
                 }
-            })
-            break;
-
+            });
         case "sinalizacao_horizontal_qte":
-            return data.gpx.wpt.reduce((sum, elem, index) => {
-                if (!!data.gpx.wpt[index + 1]) {
-                    if (!data.gpx.wpt[index + 1].name[0].includes("Remover")) {
-                        if (elem.name[0] === param) {
-                            return sum + 1
-                        } return sum
-                    } return sum
-                } return sum
-            }, 0)
-            break;
-
         case "controle_de_velocidade_qte":
-            return data.gpx.wpt.reduce((sum, elem, index) => {
-                if (!!data.gpx.wpt[index + 1]) {
-                    if (!data.gpx.wpt[index + 1].name[0].includes("Remover")) {
-                        if (elem.name[0] === param) {
-                            return sum + 1
-                        } return sum
-                    } return sum
-                } else if (elem.name[0] === param) {
-                    return sum + 1
-                } return sum
-            }, 0)
-            break;
         case "iluminacao_estrutura_qte":
             return data.gpx.wpt.reduce((sum, elem, index) => {
                 if (!!data.gpx.wpt[index + 1]) {
                     if (!data.gpx.wpt[index + 1].name[0].includes("Remover")) {
                         if (elem.name[0] === param) {
-                            return sum + 1
-                        } return sum
-                    } return sum
+                            return sum + 1;
+                        }
+                    }
                 } else if (elem.name[0] === param) {
-                    return sum + 1
-                } return sum
-            }, 0)
-            break;
-
-
-        // case adaptado para quem fez avalia;'ao no comeco e esqueceu de atualizar o app
+                    return sum + 1;
+                }
+                return sum;
+            }, 0);
         case "larguras_estrutura_qte":
             let value = 0;
             data.gpx.wpt.forEach((elem, index) => {
@@ -154,14 +159,11 @@ function parametrization(data: object, param: string, type: string) {
                         }
                     }
                 }
-            })
+            });
             return value;
-            break;
-
         default:
             break;
     }
-
 }
 
-processarTodosArquivosGpx()
+processarTodosArquivosGpx();
